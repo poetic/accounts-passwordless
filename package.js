f2f21f2e26dd4224396904584a721717c9c640c1
Leonidez Acosta <leonidez@poeticsystems.com>
2015-06-01 22:18:07 -0500

    initial commit

---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  name: 'poeticsystems:accounts-passwordless',
+  version: '0.0.1',
+  summary: 'create and login users without requiring a password',
+  git: '',
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.use('accounts-base', ['client', 'server']);
+  api.addFiles('accounts-passwordless.js');
+});
+
+Package.onTest(function(api) {
+  //api.use('tinytest');
+  //api.use('poeticsystems:accounts-passwordless');
+  //api.addFiles('accounts-passwordless-tests.js');
+});
