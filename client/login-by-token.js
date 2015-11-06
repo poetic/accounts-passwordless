04902fd9cbd6c28e492ac032e24cfd8bf2498953
John Rodney <jrodney@poeticsystems.com>
2015-11-06 14:31:06 -0600

    erraneous console.log removed

--- client/login-by-token.js
+++ client/login-by-token.js
@@ -1,5 +1,4 @@
 Accounts.loginByPhone = function(code, phoneNumber){
-  console.log(Accounts);
   Meteor.call('loginByPhone', code, phoneNumber, function(err, token){
     if(err){ return; }
     Meteor.loginWithToken(token);
