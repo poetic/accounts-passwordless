92e126a6cfc9c32d21a5aa0aae22d893569fc7c5
John Rodney <jrodney@poeticsystems.com>
2015-11-18 11:27:28 -0600

    refactor to accept callback after login

--- client/login-by-token.js
+++ client/login-by-token.js
@@ -1,6 +1,11 @@
-Accounts.loginByPhone = function(code, phoneNumber){
+Accounts.loginByPhone = function(code, phoneNumber,callback){
   Meteor.call('loginByPhone', code, phoneNumber, function(err, token){
-    if(err){ return; }
-    Meteor.loginWithToken(token);
+    if(err){
+      if(_.isFunction(callback)){
+        callback(err);
+      }
+    } else {
+      Meteor.loginWithToken(token, callback); //Really our callback should be called after Meteor.loginWithToken executed and returns a result
+    }
   });
 };
